d6daf756f384c2dd997ef4ba78c3b85bca10828f
Teresa Tavernelli <teresa.tavernelli@gmail.com>
2021-05-18 13:14:51 -0400

    una prueba

--- js/app.js
+++ js/app.js
@@ -1,17 +1,18 @@
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded', function(){
     iniciarApp();
 });
 
 function iniciarApp(){
+    console.log('Iniciando app');
     mostrarServicios();
 }
 
 async function mostrarServicios(){
     try {
         const resultado = await fetch('servicios.json');
-        const db = await resultado.json();
+        //const db = await resultado.json();
         
-        console.log(db.servicios);
+        console.log(resultado);
 
     } catch (error) {
         console.log(error);
